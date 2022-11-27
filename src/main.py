335b5e1bbaf97bf3fe5fba6ffdf86b64c70b424d
Ahmad Obeid <obeid.ahmad2001@outlook.com>
2022-11-27 08:46:20 +0200

    test: Improve testing environment

--- src/main.py
+++ src/main.py
@@ -1,14 +1,17 @@
 from encoders_services import least_significant_bit_encode, least_significant_bit_decode
+import os
+
+project_path: str = os.path.abspath("")
 
 least_significant_bit_encode(
-    cover_path="image cover path",
-    data="data path or message as string",
-    stego_path="image stego path",
+    cover_path=os.path.join(project_path, "test_files", "clean.png"),
+    data=os.path.join(project_path, "test_files", "data.txt"),
+    stego_path=os.path.join(project_path, "test_files", "stego.png"),
     file=True
 )
 
 
 least_significant_bit_decode(
-    stego_path="image stego path",
-    result_file_path="result path if file"
+    stego_path=os.path.join(project_path, "test_files", "stego.png"),
+    result_file_path=os.path.join(project_path, "test_files", "result.txt")
 )
